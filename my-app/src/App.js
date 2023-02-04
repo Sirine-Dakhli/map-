2fdeb83e5a2f65f516410d069e3277c6ab268a76
unknown <sirinedakhli01@gmail.com>
2023-02-04 18:36:19 -0500

    fixe map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,32 @@
-import {MapContainer , Marker ,Popup, TileLayer} from "react-leaflet";
-import './App.css';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.js";
+import L from "leaflet";
+import "./App.css";
+import LeafletGeocoder from "./LeafletGeocoder ";
+ import LeafletRoutingMachine from "./LeafletRoutingMachine ";
 
 function App() {
-  const position = [45.508888, -73.561668]
+  const position = [45.508888, -73.561668];
   return (
     <div className="App">
-     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+         {/* <LeafletGeocoder /> */}
+        <LeafletRoutingMachine />
+      </MapContainer>
     </div>
   );
 }
 
+let DefaultIcon = L.icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/2776/2776063.png",
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 export default App;
